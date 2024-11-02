3fc4bc7dcedbbb411b891d67ab25451a38b5bf02
Rubén <ruben.sr.apple@gmail.com>
2024-11-02 19:43:51 +0100

    Crear grupo y vista principal

---
+++ SwiftUIEjerciciosUI/SwiftUIEjerciciosUI/UI2/Tiempo.swift
@@ -0,0 +1,18 @@
+//
+//  Tiempo.swift
+//  SwiftUIEjerciciosUI
+//
+//  Created by Rubén Segura Romo on 2/11/24.
+//
+
+import SwiftUI
+
+struct Tiempo: View {
+    var body: some View {
+        Text("Aplicación de Tiempo")
+    }
+}
+
+#Preview {
+    Tiempo()
+}
