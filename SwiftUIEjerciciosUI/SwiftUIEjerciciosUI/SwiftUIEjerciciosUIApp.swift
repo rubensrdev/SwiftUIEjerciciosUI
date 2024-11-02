511e04f79ce06fa52d1cf47fa24c59e4cc22698e
Rubén <ruben.sr.apple@gmail.com>
2024-11-02 19:17:15 +0100

    Inicialización del proyecto y creación de vista home

---
+++ SwiftUIEjerciciosUI/SwiftUIEjerciciosUI/SwiftUIEjerciciosUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIEjerciciosUIApp.swift
+//  SwiftUIEjerciciosUI
+//
+//  Created by Rubén Segura Romo on 2/11/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIEjerciciosUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
