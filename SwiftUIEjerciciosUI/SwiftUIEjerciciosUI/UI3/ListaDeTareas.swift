f4efabffb6753f046ca1224ebabb34b4a195a4b9
Rubén <ruben.sr.apple@gmail.com>
2024-11-03 12:02:56 +0100

    Crear grupo y fichero ui de lista de tareas

---
+++ SwiftUIEjerciciosUI/SwiftUIEjerciciosUI/UI3/ListaDeTareas.swift
@@ -0,0 +1,18 @@
+//
+//  ListaDeTareas.swift
+//  SwiftUIEjerciciosUI
+//
+//  Created by Rubén Segura Romo on 3/11/24.
+//
+
+import SwiftUI
+
+struct ListaDeTareas: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ListaDeTareas()
+}
